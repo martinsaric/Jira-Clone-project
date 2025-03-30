80dae5a927d229eddfdde7d4119e0dfe3159adef
martinsaric <saricmartinposao@gmail.com>
2025-03-30 05:47:33 -0700

    Ignore resolved - Added new steps as comments

--- cypress/e2e/Ignore resolved.cy.js
+++ cypress/e2e/Ignore resolved.cy.js
@@ -2,14 +2,19 @@ describe('Ignore resolved', () => {
 
     it('When the user clicks on "Ignore resolved" button, the resolved tasks should be hidden', () => {
 
+        //Step1: Visit the Jira board
         cy.visit('https://jira.trungk18.com/project/board')
 
+        //Step2: Click on the "Ignore resolved" button
         cy.get('[class="ml-3"]')
         .contains('Ignore Resolved')
         .click()
 
+        //Step3: Verify that column name "Done" is displayed
         cy.get('[class="px-3 pb-4 pt-3 uppercase text-textMedium text-13 truncate"]')
         .should('contain.text', 'Done')
+
+        //Step4: Verify that the column "Done" is empty
         cy.get('div[id="Done"]')
         .children()
         .should('not.exist')
