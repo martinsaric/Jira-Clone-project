804312af6d81730fc91bf47ea7f9cca358ced28a
martinsaric <saricmartinposao@gmail.com>
2025-03-30 03:09:27 -0700

    Ignore resolved - Test case created

---
+++ cypress/e2e/Ignore resolved.cy.js
@@ -0,0 +1,8 @@
+describe('Ignore resolved', () => {
+
+    it('When the user clicks on "Ignore resolved" button, the resolved tasks should be hidden', () => {
+
+        cy.visit('https://jira.trungk18.com/project/board')
+
+    })
+})
