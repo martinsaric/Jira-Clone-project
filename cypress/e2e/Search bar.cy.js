e261a9e49c0efb47b3c6e8de7fb8b6ec71a9a49e
martinsaric <saricmartinposao@gmail.com>
2025-03-30 15:36:05 -0700

    Search bar - Added new test case for validation

--- cypress/e2e/Search bar.cy.js
+++ cypress/e2e/Search bar.cy.js
@@ -1,10 +1,23 @@
 describe('Search bar', () => {
 
-    it('Veriy that the search bar is shown', () => {
+    it('Verify that the search bar is shown', () => {
 
         cy.visit('https://jira.trungk18.com/project/board')
 
         cy.get('[class="input-container"]')
         .should('be.visible')
+    })
+
+    it('Verify that the search bar is working correctly', () => {
+
+        cy.get('[class="input-container"]')
+        .click()
+        .type('api')
+        
+        cy.get('[class="flex container mt-7"]')
+        .should('be.visible') 
+        .each(($task) => {
+            cy.wrap($task).should('contain.text', 'API')
+    })
     })
 })
