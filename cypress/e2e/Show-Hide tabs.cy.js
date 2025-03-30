a18e0a60f817fe35fd4a4ff09aba11daeeb9c3b0
martinsaric <saricmartinposao@gmail.com>
2025-03-30 02:43:12 -0700

    Show/Hide tabs added reversed test case

--- cypress/e2e/Show-Hide tabs.cy.js
+++ cypress/e2e/Show-Hide tabs.cy.js
@@ -1,10 +1,10 @@
 describe('Show/hide tabs arrow', () => {
 
-    it('When the user clicks on arrow, the tab should be shown/hidden', () => {
+    it('When the user clicks on arrow, the tab should be hidden', () => {
         
         cy.visit('https://jira.trungk18.com/project/board')
 
-        cy.get('[class="sidebar-content"]')
+        cy.get('[class="sidebar"]')
         .invoke('css', 'width')
         .then((width) => {
 
@@ -23,5 +23,26 @@ describe('Show/hide tabs arrow', () => {
                 .should('have.css', 'width', '240px')
             }
          })
+    })
+
+    it('When the user clicks on arrow, the tab should be shown', () => {
+        
+        cy.visit('https://jira.trungk18.com/project/board')
+
+        cy.get('[class="sidebar"]')
+        .should('have.css', 'width', '240px')
+        .click()
+
+        cy.get('[class="sidebar"]')
+        .then((width) => {
+            if(width === '15px') {
+                cy.get('[class="overlay"]').click()
+                cy.get('[class="sidebar"]').should('have.css', 'width', '240px')
+            }else {
+                cy.get('[class="overlay"]').click()
+                cy.get('[class="sidebar"]').should('have.css', 'width', '15px')
+            }
+            
+        })
     })
 })
