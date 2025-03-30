df17d5e83bd9ddc73b4fa21dbbba59af0b9f3dd9
martinsaric <saricmartinposao@gmail.com>
2025-03-30 11:59:34 -0700

    Quick Links - Adding new test case for verifing redirection

---
+++ cypress/e2e/Quick links.cy.js
@@ -0,0 +1,33 @@
+describe('Quick links', () => {
+
+    it('Verify that the "Tetris" quick link is displayed ', () => {
+        
+        //Step1: Visit the Jira board
+        cy.visit('https://jira.trungk18.com/project/board')
+
+        cy.get('[class="btn btn-secondary mr-2"]')
+        .contains('Tetris')
+        .should('be.visible')
+    })
+
+    it('Verify that the "Tetris" quick link redirects to the correct page', () => {
+
+        //Step1: Visit the Jira board
+        cy.visit('https://jira.trungk18.com/project/board')
+
+        //Step:2 Find a Tetris link
+        cy.get('a[href="https://tetris.trungk18.com/"]')
+        .invoke('attr', 'href')
+        .then((tetrisURL) => {
+
+            //Step3: Using origin to visit redirected Tetris page and verify that the URL is correct
+            cy.origin(tetrisURL, () => {
+                cy.visit('/')
+                cy.url().should('eql', 'https://tetris.trungk18.com/')
+            })
+            
+        })
+        
+
+    })
+})
