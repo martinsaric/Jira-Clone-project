86120b197844998a6e586de7c1d1849e879c89ee
martinsaric <saricmartinposao@gmail.com>
2025-03-30 05:31:49 -0700

    Ignore Resolved - Finished test case - working

--- cypress/e2e/Ignore resolved.cy.js
+++ cypress/e2e/Ignore resolved.cy.js
@@ -4,5 +4,14 @@ describe('Ignore resolved', () => {
 
         cy.visit('https://jira.trungk18.com/project/board')
 
+        cy.get('[class="ml-3"]')
+        .contains('Ignore Resolved')
+        .click()
+
+        cy.get('[class="px-3 pb-4 pt-3 uppercase text-textMedium text-13 truncate"]')
+        .should('contain.text', 'Done')
+        cy.get('div[id="Done"]')
+        .children()
+        .should('not.exist')
     })
 })
