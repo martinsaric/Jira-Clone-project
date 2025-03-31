2a4b6c337d636c1dfeb7ed79f338865d909b89a0
martinsaric <saricmartinposao@gmail.com>
2025-03-31 04:14:02 -0700

    Jira Board Task Filter - Added steps as comments

--- cypress/e2e/Board/Jira_board_task_filter.cy.js
+++ cypress/e2e/Board/Jira_board_task_filter.cy.js
@@ -1,13 +1,15 @@
 describe('Jira board task filter', () => {
     it('Clicking "Only Me" filter shows only tasks assigned to the logged in user', () => {
 
+        //Step1: Visit the Jira board
         cy.baseURL()
 
+        //Step2: Find and click on the "Only My Issues" filter
         cy.xpath("//span[normalize-space()='Only My Issues']")
             .contains('Only My Issues')
             .click()
 
-
+        //Step3: Verify that only tasks assigned to the logged in user are displayed
         cy.get('[class="flex container mt-7"]')
         .find('[class="avatar-container shadow-outline-white -ml-1 ng-star-inserted"]')
         .each(($task) => {
