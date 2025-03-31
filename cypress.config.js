9f4eab85ac112cd809b3ae02d26bbb6b3d73ba57
martinsaric <saricmartinposao@gmail.com>
2025-03-31 04:01:26 -0700

    Running test cases and installation of mochreporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       specPattern: "cypress/e2e/**/*.cy.js"
+
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
+
 });
